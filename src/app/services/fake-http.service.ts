89f1218d9bb8fec127d441182c624d1c298ef4e6
Jakub Kuchejda <kuchejak@fit.cvut.cz>
2023-11-13 17:47:23 +0100

    Remove fake request logs

--- src/app/services/fake-http.service.ts
+++ src/app/services/fake-http.service.ts
@@ -8,7 +8,6 @@ export class FakeHttpService {
   constructor() { }
 
   async send<T>(type: string, url: string, response: T, body?: any): Promise<T> {
-    console.log("fake request", {  url, type, body, response });
     return response;
   }
 }
